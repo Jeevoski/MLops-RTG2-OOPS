e95495e2e5db816e9312d90089f7f6a1c384a29c
Jeevoski <jeevangeorgejohn2@gmail.com>
2026-06-04 02:02:29 +0530

    signup and sign in module

--- oops_proj.py
+++ oops_proj.py
@@ -12,15 +12,43 @@ class chatbook:
                         3.press 3 to post
                         4.press 4 to chat
                         5.press any key to signout""")
-        if user_input==1:
+        if user_input=="1":
+            self.signup()
+        elif user_input=="2":
+            self.signin()
+        elif user_input=="3":
             pass
-        elif user_input==2:
-            pass
-        elif user_input==3:
-            pass
-        elif user_input==4:
+        elif user_input=="4":
             pass
         else:
             exit()
+
+    def signup(self):
+        email=input("please enter your email")
+        pwd = input("please enter your password")
+        self.username=email
+        self.password=pwd
+        print("you have signed in successfully")
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            uname = input("enter your email/username here -> ")
+            pwd = input("enter your password here -> ")
+
+        if self.username == uname and self.password == pwd:
+            print("You have signed in successfully !!")
+            self.loggedin = True
+        else:
+            print("please input correct credentials..")
+
+        print("\n")
+        self.menu()
+
 obj=chatbook()
+
+
+
     
